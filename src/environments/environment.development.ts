39015d317d16ebc16529bf148b0ae102a77cb708
mahmoud140106 <amer140106@gmail.com>
2026-02-13 02:04:07 +0200

    last update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
   // baseUrl: 'https://localhost:7025/api',
   // base: "https://localhost:7025",
-  baseUrl: 'http://airbnb-properties.runasp.net/api',
-  base: "http://airbnb-properties.runasp.net",
+  baseUrl: 'https://itiairbnb.runasp.net/api',
+  base: "https://itiairbnb.runasp.net",
   domainBaseUrl: "http://localhost:4200",
   production: true
 };
